9cd000f8c0f2d7f818833561d7c286f739aff173
zhiguang <936246674@qq.com>
2020-12-15 11:50:15 +0800

    add .

---
+++ src/lock/Instance.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Instance
+{
+    private static $getInstance;
+
+    private function __construct(){}
+
+    private function __clone()
+    {
+        // TODO: Implement __clone() method.
+    }
+
+    public static function getInstance ($class)
+    {
+        if (self::$getInstance instanceof $class) {
+            self::$getInstance = new $class();
+        }
+
+        return self::$getInstance;
+    }
+
+}
---
+++ src/lock/RedisLock.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class RedisLock
+{
+    public $redis;
+
+    public function __construct()
+    {
+        $self = Instance::getInstance(self::class);
+        $self->redis = new Redis();
+        $self->redis = $self->redis->connect('127.0.0.1',6379,30);
+    }
+
+    public function setLock()
+    {
+        $res = $this->redis->setNx('aaa','bbb');
+    }
+}
